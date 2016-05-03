a5a911d91bc8d4de6ba12566b1fb1c7eb3160a5f
will-thurman <wct8tc@mst.edu>
2016-05-02 21:18:25 -0500

    Update search.php
    Changed the first check to file name as submit fails, added in a query using the retrieved filename

--- search.php
+++ search.php
@@ -1,11 +1,41 @@
 <?php
 include "connect.php";
-if(isset($_POST['submit'])) {
+if(isset($_POST['filename'])) {
     if (isset($_GET['go'])) {
         if (preg_match("/^[  a-zA-Z]+/", $_POST['filename'])) {
             $file_name = $_POST['filename'];
-        }
+            echo "Search: " . $file_name . "<br>";
+		}
+		else
+		{
+			//echo "preg_match<br>";
+			die();
+		}
+      }
+      else
+	  {
+		  //echo "isset(_GET['go'])<br>";
+		  die();
+      }
     }
-}
+	else
+	{
+		echo "isset POST<br>";
+		die();
+	}
+	
+	$query = "SELECT user_id, fileName FROM file_tbl WHERE fileName LIKE '%$file_name%'";
+	//echo $query . "<br>";
+            $result = mysqli_query($dbconnection, $query);
+			if(mysqli_num_rows($result) > 0)
+			{
+				while($row = mysqli_fetch_assoc($result))
+				{
+					echo "User: " . $row["user_id"] . " - File Name: " . $row["fileName"]."<br>";
+				}
+			}
+			else
+				echo "0 results";
 
+  mysqli_close($dbconnection);
 ?>
