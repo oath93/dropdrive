1b136eb312bd55bf9bf2a2d7089abdf0b493bbba
Brenden <bkb9tb@mst.edu>
2016-05-04 13:57:22 -0500

    Added logo.png Added "test_user" logic for upload.php     test_user is assigned to the session as a user if there is not a current session user when trying to upload files

--- upload.php
+++ upload.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <link rel="stylesheet", type="text/css", href="base.css">
+    <link rel="stylesheet" , type="text/css" , href="base.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Main Page</title>
 </head>
 
 <body>
 <div class="login">
-
 </div>
 
-<div id = "main_content" style=margin-top: 5%;">
+<div id = "main_content" style='margin-top:5%;'>
     <br />
     <p>
         <?php
         include"connect.php";
+        if(empty($_SESSION['Username'])){
+            $_SESSION['Username'] = "Test_User";
+        }
         $target_dir = "uploads/".$_SESSION['Username']."/";
         if(isset($_POST['submit'])){
             $ext_start = $nameLength = strlen($_FILES['upload']['name']);
             $captured_name = $_FILES['upload']['name'];
             $found_ext_start = false;
             do{
-                if($captured_name[$ext_start-1] != '.')
+                if($captured_name[$ext_start - 1] != '.')
                 {
                     $ext_start --;
                 }else{
@@ -39,12 +41,11 @@
             $fileSize = $_FILES['upload']['size'];
             $fileType = $_FILES['upload']['type'];
             $target_file = $target_dir.$fileName;
-            echo $fileName.".".$extension;
             if(!is_dir($target_dir)){
                 mkdir($target_dir);
             }
             if (move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file.".".$extension)) {
-                echo "The file ". $fileName. " has been uploaded.";
+                echo "<h3 style='margin-left: 25%;'>The file ". $fileName.".".$extension." has been uploaded!</h3>";
             }
             else{
                 echo "The file could not be uploaded.";
@@ -52,10 +53,13 @@
         }
         ?>
     </p>
-    <br />
-    <br />
-    <br />
-    <br />
+
+    <p style='margin-left: 25%;'>Would you like to upload another file?
+    <br/>
+    <a href="upload.html" class="button">Upload Another</a>
+        <a href="userpage.php" class="button">Return to User Page</a>
+        <a href="index.php" class="button">Return Home</a>
+    </p>
 </div>
 </body>
 </html>
