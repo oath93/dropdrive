07c5d566a416d4daf2677a89b6c9ac631e620356
William Thurman <William Thurman>
2016-05-06 11:24:34 -0500

    added deletion function to userpage

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+  include "connect.php";
+  $file= htmlspecialchars($_GET["filename"]);
+  $file_dir="uploads/".$_SESSION['Username']."$file";
+  
+  $query = "Delete From file_tbl Where fileName='$file' And user_id='".$_SESSION['Username']."'";
+  if(mysqli_query($connection, $query))
+  {
+	  echo "File Deleted from Database<br/>";
+  }
+  sleep(1);
+  header("Location: http://localhost/userpage.php");
+  die();
+?>
